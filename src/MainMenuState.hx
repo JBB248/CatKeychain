4be3c76b75f7bde48e4a2156ee22763398c26e63
JBB248 <joebray64@gmail.com>
2024-04-23 13:48:32 -0500

    Fix button placement bug

--- src/MainMenuState.hx
+++ src/MainMenuState.hx
@@ -13,6 +13,7 @@ import flixel.math.FlxPoint;
 import flixel.sound.FlxSound;
 import flixel.text.FlxText;
 import flixel.tweens.FlxEase;
+import flixel.ui.FlxButton;
 
 class MainMenuState extends FlxTransitionableState
 {
@@ -64,41 +65,43 @@ class MainMenuState extends FlxTransitionableState
 
         springSFX = new FlxSound().loadEmbedded(new Assets.SpringSound());
         
-        var titleText = new FlxText(20, 20, FlxG.width * 0.5 - 40, "Cat Gallery", 60);
+        var titleText = new FlxText(20, 40, FlxG.width * 0.5 - 40, "Cat Gallery", 60);
         titleText.color = 0xFF000000;
 
-        var galleryButton = new FlxButtonPlus(20, titleText.height + 20, () -> FlxG.switchState(PlayState.new), "Gallery");
-        galleryButton.updateInactiveButtonColors([0xFF414141, 0xFF000000]);
-		galleryButton.updateActiveButtonColors([0xFFFF0000, 0xFF800000]);
+        var galleryButton = new FlxButton(22, titleText.y +  titleText.height + 15, "Gallery", () -> FlxG.switchState(AltState.new));
+        var browseButton = new FlxButton(galleryButton.x + galleryButton.width + 10, galleryButton.y, "Browse", () -> FlxG.switchState(PlayState.new));
+        var creditsButton = new FlxButton(browseButton.x + browseButton.width + 10, galleryButton.y, "Credits", () -> {/* Play SFX */});
 
-        // add(new FlxBackdrop(Assets.backdropTile));
         add(spinningCat);
         add(titleText);
         add(galleryButton);
+        add(browseButton);
+        add(creditsButton);
     }
 
     var catBounceElapsed:Float = 1.8;
+    var deltaX:Float = 0;
+    var ratioY:Float = 1;
+
     override public function update(elapsed:Float):Void
     {
         super.update(elapsed);
 
-        catBounceElapsed += elapsed;
-
-        if(FlxG.keys.justPressed.SPACE)
-            FlxG.switchState(AltState.new);
-
-        else if(FlxG.mouse.justPressed && FlxG.mouse.overlaps(spinningCat))
+        if(FlxG.mouse.justPressed && FlxG.mouse.overlaps(spinningCat))
         {
             springSFX.play(true);
             catBounceElapsed = 0.0;
+            deltaX = FlxG.random.float(-0.2, 0.2);
+            ratioY = FlxG.random.floatNormal(spinningCat.frameHeight / spinningCat.frameWidth, 0.2);
         }
-
         if(catBounceElapsed < 1.8)
         {
+            catBounceElapsed += elapsed;
+
             var wave = catSize * FlxEase.elasticOut(catBounceElapsed + 0.05);
 
-            spinningCat.scale.x = Math.abs(wave / spinningCat.frameWidth);
-            spinningCat.scale.y = Math.abs(wave / spinningCat.frameHeight);
+            spinningCat.scale.x = Math.abs((wave + deltaX) / spinningCat.frameWidth);
+            spinningCat.scale.y = Math.abs(wave * ratioY / spinningCat.frameHeight);
             spinningCat.updateHitbox();
             spinningCat.x = FlxG.width * 0.75 - spinningCat.width * 0.5;
             spinningCat.y = FlxG.height * 0.5 - spinningCat.height * 0.5;
