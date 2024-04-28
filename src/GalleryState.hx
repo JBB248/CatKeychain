bf58a1dbe7a8de908080b6f194cef8f43e74189e
JBB248 <joebray64@gmail.com>
2024-04-28 15:35:18 -0500

    Add substate for photo isolation

--- src/GalleryState.hx
+++ src/GalleryState.hx
@@ -5,20 +5,20 @@ import flixel.FlxObject;
 import flixel.FlxSprite;
 import flixel.FlxSubState;
 import flixel.addons.transition.FlxTransitionableState;
-import flixel.group.FlxContainer.FlxTypedContainer;
-import flixel.input.keyboard.FlxKey;
+import flixel.group.FlxGroup;
 import flixel.input.mouse.FlxMouseEvent;
 import flixel.math.FlxMath;
-import flixel.system.FlxAssets.FlxGraphicAsset;
-
-import openfl.events.KeyboardEvent;
-import openfl.events.MouseEvent;
+import flixel.system.FlxAssets;
+import flixel.util.FlxDestroyUtil;
 
 class GalleryState extends FlxTransitionableState
 {
     public var camTarget:FlxObject;
 
-    public var gallery:FlxTypedContainer<GalleryPhoto>;
+    public var gallery:FlxTypedGroup<GalleryPhoto>;
+    public var focus(default, set):GalleryPhoto;
+
+    public var viewSubState:GallerySubState;
 
     public function new() 
     {
@@ -41,7 +41,12 @@ class GalleryState extends FlxTransitionableState
         }
         else
         {
-            gallery = new FlxTypedContainer();
+            camTarget = new FlxObject(0, 0, FlxG.width, FlxG.height);
+            viewSubState = new GallerySubState();
+            destroySubStates = false;
+
+            gallery = new FlxTypedGroup(); // FlxContainer has a child/parent relationship that bothers the substate
+            add(gallery);
 
             var matrix = [[]];
             var row = 0;
@@ -60,7 +65,7 @@ class GalleryState extends FlxTransitionableState
                 matrix[matrix.length - 1].push(photo);
             }
 
-            for(j => row in matrix)
+            for(j => row in matrix) // Find a way to remove this
             {
                 var dx = (FlxG.width - Lambda.fold(row, (item, result) -> result + item.width, 0)) / 2;
                 for(i => photo in row)
@@ -73,23 +78,19 @@ class GalleryState extends FlxTransitionableState
 
                     photo.y = FlxG.height / 3 * j;
                     photo.updateCenter();
-                    add(photo);
+                    gallery.add(photo);
                 }
             }
 
-            camTarget = new FlxObject(0, 0, FlxG.width, FlxG.height);
-
-            FlxG.camera.setScrollBounds(0, FlxG.width, 0, GalleryPhoto.photoHeight * matrix.length);
             FlxG.worldBounds.set(0, 0, FlxG.width, GalleryPhoto.photoHeight * matrix.length);
+            FlxG.camera.setScrollBounds(0, FlxG.width, 0, GalleryPhoto.photoHeight * matrix.length);
             FlxG.camera.follow(camTarget, NO_DEAD_ZONE, 0.5);
         }
     }
 
     public function isolatePhoto(photo:GalleryPhoto):Void
     {
-        remove(photo, true);
-
-        // openSubState(new GallerySubstate(photo));
+        openSubState(viewSubState.reset(photo));
     }
 
     function findPhoto():Void
@@ -103,15 +104,63 @@ class GalleryState extends FlxTransitionableState
 
         final keys = FlxG.keys;
         if(keys.justReleased.F && keys.pressed.CONTROL)
+        {
             findPhoto();
+        }
         else if(keys.justReleased.ESCAPE)
+        {
             FlxG.switchState(MainMenuState.new);
+        }
 
-        camTarget.y -= FlxG.mouse.wheel * 30;
+        // Update scroll
+        camTarget.y -= FlxG.mouse.wheel * 40;
         if(camTarget.y < FlxG.worldBounds.y)
             camTarget.y = 0;
         else if(camTarget.y + camTarget.height > FlxG.worldBounds.height)
             camTarget.y = FlxG.worldBounds.height - camTarget.height;
+    }
+
+    override public function closeSubState():Void
+    {
+        super.closeSubState();
+
+        focus = null; // Make sure the photo resets, even if it's still being targeted
+    }
+
+    override public function destroy():Void
+    {
+        super.destroy();
+
+        viewSubState = FlxDestroyUtil.destroy(viewSubState);
+    }
+
+    @:noCompletion function set_focus(value:GalleryPhoto):GalleryPhoto
+    {
+        if(focus != null)
+        {
+            // Restore old focus to original size
+            focus.setGraphicSize(0, GalleryPhoto.photoHeight);
+            focus.updateHitbox();
+
+            focus.x = focus.centerX - focus.width * 0.5;
+            focus.y = focus.centerY - focus.height * 0.5;
+        }
+
+        if(value != null)
+        {
+            // Enlarge new focus to make it stand out
+            value.setGraphicSize(0, GalleryPhoto.photoHeight * 1.25);
+            value.updateHitbox();
+
+            value.x = value.centerX - value.width * 0.5;
+            value.y = value.centerY - value.height * 0.5;
+
+            // Move new focus to top of draw stack
+            final members = gallery.members;
+            members.push(members.splice(members.indexOf(value), 1)[0]);
+        }
+
+        return focus = value;
     }
 }
 
@@ -119,12 +168,25 @@ class GallerySubState extends FlxSubState
 {
     public var photo:GalleryPhoto;
 
-    public function new(photo:GalleryPhoto)
+    public function new()
     {
         super(0x88000000);
+    }
 
-        this.photo = photo;
+    override public function create():Void
+    {
+        // Add ui stuff here
+    }
+
+    public function reset(newPhoto:GalleryPhoto):GallerySubState
+    {
+        if(photo != null)
+            remove(photo);
+
+        photo = newPhoto;
         add(photo);
+
+        return this;
     }
 
     override public function update(elapsed:Float):Void
@@ -132,15 +194,7 @@ class GallerySubState extends FlxSubState
         super.update(elapsed);
 
         if(FlxG.keys.justReleased.ESCAPE)
-        {
-            remove(photo); // Don't let it get destroyed
             close();
-        }
-    }
-
-    override public function close():Void
-    {
-        super.close();
     }
 }
 
@@ -165,18 +219,13 @@ class GalleryPhoto extends FlxSprite
         setGraphicSize(0, photoHeight);
         updateHitbox();
 
-        FlxMouseEvent.add(this, onDown, onUp, onOver, onOut, false, true, false);
+        FlxMouseEvent.add(this, null, onUp, onOver, onOut, false, true, false);
     }
 
     public function updateCenter():Void
     {
         centerX = x + width * 0.5;
         centerY = y + height * 0.5;
-    }
-
-    function onDown(_):Void
-    {
-
     }
 
     function onUp(_):Void
@@ -190,24 +239,13 @@ class GalleryPhoto extends FlxSprite
     {
         if(gallery.subState != null) return;
 
-        setGraphicSize(0, photoHeight * 1.25);
-        updateHitbox();
-
-        x = centerX - width * 0.5;
-        y = centerY - height * 0.5;
-
-        gallery.remove(this, true);
-        gallery.add(this);
+        gallery.focus = this; // Look at me!
     }
 
     function onOut(_):Void
     {
         if(gallery.subState != null) return;
 
-        setGraphicSize(0, photoHeight);
-        updateHitbox();
-
-        x = centerX - width * 0.5;
-        y = centerY - height * 0.5;
+        gallery.focus = null; // Hovering over whitespace
     }
 }
